655fea08bffd4da45db74cc7eda2293fe91c2edb
Your Name <you@example.com>
2020-05-09 22:50:26 +0200

    specialite js/ajax checkbox button
    tant que l'utilisateur ne clique pas sur au moins une checkbox, le bouton suivant ne s'affichera pas: done. seul hic c'est que le bouton de gauche apparait tout en haut tant que le user n'a pas checkbox

---
+++ specialites.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+   var boxes = $(' input[type=checkbox]').on('change', function() {
+    var flag = boxes.filter(':checked').length > 0;
+
+    $('#next-container').toggle(flag);
+  });
+});
