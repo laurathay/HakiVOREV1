6668093af56ae24cb41d1f2fb4ddaaad687c2fb1
Laura THAY <laura.thay@ipag.fr>
2020-05-10 15:48:39 +0200

    bouton geolocalisation fixed
    OH YEAH

---
+++ transition.js
@@ -0,0 +1,32 @@
+$(document.body).ready(function() {
+$('body'.justeUnMoment.html).css('display', 'none');
+$('body'.index.html).fadeOut(1000);
+
+$('.link').setTimeout(function(appear) {
+event.preventDefault();
+newLocation = this.justeUnMoment.html;
+$('body').fadeIn(1000, newpage);
+});
+
+
+
+function newpage() {
+window.location = newLocation;
+}
+});
+
+//$(document).ready(function() {
+// $('body').css('display', 'none');
+// $('body').fadeIn(1000);
+// $('.link').click(function(event) {
+// event.preventDefault();
+// newLocation = this.href;
+// $('body').fadeOut(1000, newpage);
+// });
+//
+//
+//
+// function newpage() {
+// window.location = newLocation;
+// }
+// });
